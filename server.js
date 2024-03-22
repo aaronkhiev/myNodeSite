04b42434b5bc5087e7b38977ffa24366151acdfe
Aaron <aaron_khiev@uri.edu>
2024-03-21 20:31:38 -0400

    NodeJS Converted Client Site

--- server.js
+++ server.js
@@ -30,25 +30,77 @@ http.createServer(function(req, res) {
         case '':
             serveStaticFile(res, '/public/index.html', 'text/html');
             break;
-        
+
         case '/index':
             serveStaticFile(res, '/public/index.html', 'text/html');
             break;
 
-        case '/contact':
-            serveStaticFile(res, '/public/contact.html', 'text/html');
+        case '/ddpage':
+            serveStaticFile(res, '/public/ddpage.html', 'text/html');
             break;
 
-        case '/posts':
-            serveStaticFile(res, '/public/posts.html', 'text/html');
+        case '/generate':
+            serveStaticFile(res, '/public/generate.html', 'text/html');
             break;
 
-        case '/under-construction':
-            serveStaticFile(res, '/public/under-construction.html', 'text/html');
+        case '/character':
+            serveStaticFile(res, '/public/character.html', 'text/html');
             break;
 
-        case '/css/style.css':
-            serveStaticFile(res, '/public/css/style.css', 'text/css');
+        case '/about':
+            serveStaticFile(res, '/public/about.html', 'text/html');
+            break;
+
+        case '/action':
+            serveStaticFile(res, '/public/action.html', 'text/html');
+            break;
+
+        case '/skills':
+            serveStaticFile(res, '/public/skills.html', 'text/html');
+            break;
+        
+        case '/spell':
+            serveStaticFile(res, '/public/spell.html', 'text/html');
+            break;
+
+        case '/css/accord.css':
+            serveStaticFile(res, '/public/css/accord.css', 'text/css');
+            break;
+
+        case '/css/button.css':
+            serveStaticFile(res, '/public/css/button.css', 'text/css');
+            break;
+
+        case '/css/character.css':
+            serveStaticFile(res, '/public/css/character.css', 'text/css');
+            break;
+
+        case '/css/comp.css':
+            serveStaticFile(res, '/public/css/comp.css', 'text/css');
+            break;
+
+        case '/css/dice.css':
+            serveStaticFile(res, '/public/css/dice.css', 'text/css');
+            break;
+
+        case '/css/format.css':
+            serveStaticFile(res, '/public/css/format.css', 'text/css');
+            break;
+
+        case '/css/navigation.css':
+            serveStaticFile(res, '/public/css/navigation.css', 'text/css');
+            break;
+
+        case '/css/npc.css':
+            serveStaticFile(res, '/public/css/npc.css', 'text/css');
+            break;
+
+        case '/css/skillsstyle.css':
+            serveStaticFile(res, '/public/css/skillsstyle.css', 'text/css');
+            break;
+
+        case '/css/spells.css':
+            serveStaticFile(res, '/public/css/spells.css', 'text/css');
             break;
 
         case '/images/404bottom.gif':
@@ -59,36 +111,128 @@ http.createServer(function(req, res) {
             serveStaticFile(res, '/public/images/404mid.gif', 'image/gif');
             break;
 
-        case '/images/404bottom.jpg':
+        case '/images/404top_w.jpg':
             serveStaticFile(res, '/public/images/404top_w.jpg', 'image/jpg');
-            break;
-
-        case '/images/blogging.png':
-            serveStaticFile(res, '/public/images/blogging.png', 'image/png');
-            break;
-
-        case '/images/computer-typing.jpeg':
-            serveStaticFile(res, '/public/images/computer-typing.jpeg', 'image/jpeg');
-            break;
-
-        case '/images/construction.png':
-            serveStaticFile(res, '/public/images/construction.png', 'image/png');
-            break;
-        
-        case '/images/logo.png':
-            serveStaticFile(res, '/public/images/logo.png', 'image/png');
-            break;
-
-        case '/images/merch.png':
-            serveStaticFile(res, '/public/images/merch.png', 'image/png');
             break;
 
         case '/images/x.png':
             serveStaticFile(res, '/public/images/x.png', 'image/png');
             break;
 
+        case '/images/ttlogo.png':
+            serveStaticFile(res, '/public/images/ttlogo.png', 'image/png');
+            break;
+
+        case '/images/d4.png':
+            serveStaticFile(res, '/public/images/d4.png', 'image/png');
+            break;
+
+        case '/images/d12.png':
+            serveStaticFile(res, '/public/images/d12.png', 'image/png');
+            break;
+
+        case '/images/d20.png':
+            serveStaticFile(res, '/public/images/d20.png', 'image/png');
+            break;
+
+        case '/images/d20p.png':
+            serveStaticFile(res, '/public/images/d20p.png', 'image/png');
+            break;
+
+        case '/images/d20resized.png':
+            serveStaticFile(res, '/public/images/d20resized.png', 'image/png');
+            break;
+
+        case '/images/finn.png':
+            serveStaticFile(res, '/public/images/finn.png', 'image/png');
+            break;
+
+        case '/images/fireball.png':
+            serveStaticFile(res, '/public/images/fireball.png', 'image/png');
+            break;
+
+        case '/images/heal.png':
+            serveStaticFile(res, '/public/images/heal.png', 'image/png');
+            break;
+
+        case '/images/nora.png':
+            serveStaticFile(res, '/public/images/nora.png', 'image/png');
+            break;
+
+        case '/images/shield.png':
+            serveStaticFile(res, '/public/images/shield.png', 'image/png');
+            break;
+
+        case '/images/tenti.png':
+            serveStaticFile(res, '/public/images/tenti.png', 'image/png');
+            break;
+
+        case '/images/thistle.png':
+            serveStaticFile(res, '/public/images/thistle.png', 'image/png');
+            break;
+
+        case '/images/verdan.png':
+            serveStaticFile(res, '/public/images/verdan.png', 'image/png');
+            break;
+
+        case '/js/jquery-ui-1.13.2.custom/jquery-ui-1.13.2.custom/jquery-ui.js':
+            serveStaticFile(res, '/public/js/jquery-ui-1.13.2.custom/jquery-ui-1.13.2.custom/jquery-ui.js', 'text/javascript');
+            break;
+
+        case '/js/accordian.js':
+            serveStaticFile(res, '/public/js/accordian.js', 'text/javascript');
+            break;
+
+        case '/js/ajax-html.js':
+            serveStaticFile(res, '/public/js/ajax-html.js', 'text/javascript');
+            break;
+
+        case '/js/ajax-jquery.js':
+            serveStaticFile(res, '/public/js/ajax-jquery.js', 'text/javascript');
+            break;
+
+        case '/js/ajax-json.js':
+            serveStaticFile(res, '/public/js/ajax-json.js', 'text/javascript');
+            break;
+
+        case '/js/ajax-xml.js':
+            serveStaticFile(res, '/public/js/ajax-xml.js', 'text/javascript');
+            break;
+
+        case '/js/character.js':
+            serveStaticFile(res, '/public/js/character.js', 'text/javascript');
+            break;
+
+        case '/js/dice.js':
+            serveStaticFile(res, '/public/js/dice.js', 'text/javascript');
+            break;
+
+        case '/js/jquery-3.7.1.min.js':
+            serveStaticFile(res, '/public/js/jquery-3.7.1.min.js', 'text/javascript');
+            break;
+
+        case '/data/spell-jquery.html':
+            serveStaticFile(res, '/public/data/spell-jquery.html', 'text/html');
+            break;
+
+        case '/data/spells.xml':
+            serveStaticFile(res, '/public/data/spells.xml', 'application/xml');
+            break;
+
+        case '/data/npc-data.json':
+            serveStaticFile(res, '/public/data/npc-data.json', 'application/json');
+            break;
+
+        case '/data/this-data.html':
+            serveStaticFile(res, '/public/data/this-data.html', 'text/html');
+            break;
+
+        case '/data/ver-data.html':
+            serveStaticFile(res, '/public/data/ver-data.html', 'text/html');
+            break;
+
         default:
-            serveStaticFile(res, '/public/404.html', 'text/html', 404)
+            serveStaticFile(res, '/public/html/404.html', 'text/html', 404)
             break;
     }
 }).listen(PORT);
